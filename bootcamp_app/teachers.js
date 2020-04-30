72bcf0307d91995366fcb466c442e5ddfa18fd9c
Ron Juarez <ronstewartjuarez@gmail.com>
2020-04-30 09:54:36 -0400

    paramemerized query strings

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,18 +6,23 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const cohortName = process.argv[2];
+ 
+const values = [`%${cohortName}%`];
 
-pool.query(`
+const text = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
+WHERE cohorts.name = $1
+ORDER BY teacher;`
+
+pool
+.query(text, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
-});
+}).catch(err => console.error('query error', err.stack));
